ecc39b64ee8ce204e21f7096b2486c7e690e64c9
AgustinPalmaM <palma.agustin7@gmail.com>
2023-08-03 17:36:35 -0400

    Add test to Car controller indes and show methods

--- test/controllers/cars_controller_test.rb
+++ test/controllers/cars_controller_test.rb
@@ -2,12 +2,23 @@ require "test_helper"
 
 class CarsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get cars_index_url
+    get cars_path
     assert_response :success
+    assert_select ".car", 2
   end
 
   test "should get show" do
-    get cars_show_url
+    get car_path(cars(:one))
     assert_response :success
+    assert_select ".registration", "FFCF53"
+    assert_select ".brand", "Kia Motors"
+    assert_select ".version", "Rio"
+    assert_select ".model", "Rio 3 1.4 Mec"
+    assert_select ".year", "2013"
+    assert_select ".color",  "Plateado plata"
+    assert_select ".price",  "7500000"
+    assert_select ".transmission",  "Mec"
+    assert_select ".traction", "4X2"
+    assert_select ".fuel", "Gasolina"
   end
 end
