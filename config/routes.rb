b36970bf5fa1a211d597f5916fe023d2fe78e693
AgustinPalmaM <palma.agustin7@gmail.com>
2023-08-02 23:14:39 -0400

    add products index route add products controller with method index add products view index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'products/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/products', to: 'products#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
